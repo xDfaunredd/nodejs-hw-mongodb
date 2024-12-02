5c0ef59fc023882bf3f6684f0f0c340ca275e963
xd.faunredd <andrew.furevych@gmail.com>
2024-12-02 18:17:40 +0200

    bug fixes

--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -9,7 +9,7 @@ export const createContactSchema = Joi.object({
   }),
   phoneNumber: Joi.string().min(3).max(20).required(),
   email: Joi.string().email().required(),
-  isFavorite: Joi.boolean(),
+  isFavourite: Joi.boolean(),
   contactType: Joi.string().valid('work', 'home', 'personal').required(),
 });
 
@@ -17,6 +17,6 @@ export const updateContactSchema = Joi.object({
   name: Joi.string().min(3).max(20),
   phoneNumber: Joi.string().min(3).max(20),
   email: Joi.string().email(),
-  isFavorite: Joi.boolean(),
+  isFavourite: Joi.boolean(),
   contactType: Joi.string().valid('work', 'home', 'personal'),
 });
