08c852f7236b978456669c132635408d3b3e1a3f
xd.faunredd <andrew.furevych@gmail.com>
2024-12-16 20:58:31 +0200

    add files

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-// src/index.js
-
 import { TEMP_UPLOAD_DIR, UPLOAD_DIR } from './constants/index.js';
 import { initMongoConnection } from './db/initMongoConnection.js';
 import { setupServer } from './server.js';
