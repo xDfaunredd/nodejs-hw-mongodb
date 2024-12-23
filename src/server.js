cbcafb4ff14a351fe163f3f912f74fff71d0ab4c
xd.faunredd <andrew.furevych@gmail.com>
2024-12-23 20:25:39 +0200

    add working swagger files

--- src/server.js
+++ src/server.js
@@ -8,6 +8,7 @@ import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import router from './routes/index.js';
 import cookieParser from 'cookie-parser';
 import { UPLOAD_DIR } from './constants/index.js';
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
 
 dotenv.config();
 
@@ -20,6 +21,7 @@ export const setupServer = () => {
   app.use(cors());
   app.use(cookieParser());
   app.use('/uploads', express.static(UPLOAD_DIR));
+  app.use('/api-docs', swaggerDocs());
 
   app.use(
     pino({
